8581d16d4a3083bbdf5a622e433a9246483f81eb
Heitor Kuba <heitor.kuba@arquivei.com.br>
2020-07-31 23:43:08 -0300

    Add basic page conected to FIPE API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+import { Marca } from '../types';
 
-export default function Home() {
-  return <h1>Hello World</h1>;
+interface HomePageProps {
+  brands: Marca[];
 }
+
+function HomePage({ brands }: HomePageProps) {
+  return (
+    <ul>
+      {brands.map((brand) => (
+        <li key={brand.codigo}>{brand.nome}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getServerSideProps() {
+  const { data: brands } = await axios.get(
+    'https://parallelum.com.br/fipe/api/v1/carros/marcas',
+  );
+  return {
+    props: { brands },
+  };
+}
+
+export default HomePage;
---
+++ types.ts
@@ -0,0 +1,4 @@
+export interface Marca {
+  codigo: number;
+  nome: string;
+}
